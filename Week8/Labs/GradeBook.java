5f4661f11ba00fd28c211871a1bd9c54d78bdb14
Dan Schaper <dschaper@ganymeade.com>
2013-10-15 19:31:47 -0700

    Week 8 Lab 1

---
+++ Week8/Labs/GradeBook.java
@@ -0,0 +1,11 @@
+/**
+ * @author Dan Schaper
+ *         Date: 10/15/13
+ *         Time: 7:28 PM
+ */
+public class GradeBook {
+
+    public void displayMessage() {
+        System.out.println("Welcome to the Grade Book!");
+    }
+}
---
+++ Week8/Labs/GradeBookTester.java
@@ -0,0 +1,12 @@
+/**
+ * @author Dan Schaper
+ *         Date: 10/15/13
+ *         Time: 7:30 PM
+ */
+public class GradeBookTester {
+
+    public static void main(String[] args) {
+        GradeBook myGradebook = new GradeBook();
+        myGradebook.displayMessage();
+    }
+}
