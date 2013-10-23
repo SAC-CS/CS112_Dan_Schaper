2b6f83501b4242c8dce742166a58c8b264ddfe62
Dan Schaper <dschaper@ganymeade.com>
2013-10-22 19:39:49 -0700

    Week 9 Lab 1

---
+++ Week9/Labs/OddOrEven.java
@@ -0,0 +1,16 @@
+/**
+ * @author Dan Schaper
+ *         Date: 10/22/13
+ *         Time: 7:36 PM
+ *         Week 9 Lab 1
+ */
+public class OddOrEven {
+
+    public void odd() {
+        System.out.println("Odd");
+    }
+
+    public void even() {
+        System.out.println("Even");
+    }
+}
---
+++ Week9/Labs/OddOrEvenTester.java
@@ -0,0 +1,13 @@
+/**
+ * @author Dan Schaper
+ *         Date: 10/22/13
+ *         Time: 7:38 PM
+ *         Week 9 Lab 1
+ */
+public class OddOrEvenTester {
+    public static void main(String[] args) {
+        OddOrEven game = new OddOrEven();
+        game.odd();
+        game.even();
+    }
+}
