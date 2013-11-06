ca5b21811919470cd2afadc905ad0cc78a2f0b1f
Dan Schaper <dschaper@ganymeade.com>
2013-11-05 21:41:00 -0800

    Week 11 Lab 8

--- Week11/Labs/TrippleDiceWithHistoryTester.java
+++ Week11/Labs/TrippleDiceWithHistoryTester.java
@@ -6,10 +6,13 @@
 public class TrippleDiceWithHistoryTester {
     public static void main(String[] args) {
         TrippleDiceWithHistory T1 = new TrippleDiceWithHistory();
+        TrippleDice T2 = new TrippleDice();
         for (int i = 0; i < 10; i++) {
             T1.Throw();
+            T2.Throw();
         }
         System.out.println(T1);
+        System.out.println(T2);
 
     }
 }
