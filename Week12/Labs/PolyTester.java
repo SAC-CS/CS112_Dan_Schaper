6aa01f875d95dc75be561922eeb2d6abc2b34f86
Dan Schaper <dschaper@ganymeade.com>
2013-11-12 20:25:11 -0800

    Week 12 Lab 2

--- Week12/Labs/PolyTester.java
+++ Week12/Labs/PolyTester.java
@@ -7,11 +7,14 @@ public class PolyTester {
     public static void main(String[] args) {
         DoubleDice dDice = new DoubleDice();
         TrippleDice tDice = new TrippleDice();
+        Dice sDice = new DoubleDice();
 
         System.out.println(dDice);
         System.out.println(dDice.Throw());
         System.out.println(tDice);
         System.out.println(tDice.Throw());
+        System.out.println(sDice);
+        System.out.println(sDice.Throw());
 
     }
 }
